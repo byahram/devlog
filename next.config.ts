cfb074c231ce579653fc86db779b872378a1d65a
Ahram Kim <ahram0223@gmail.com>
2025-09-29 01:48:33 +0900

    feat: GitHub Pages 자동 배포 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx"],
+  compiler: {
+    styledComponents: true,
+  },
+  assetPrefix: isProd ? "https://byahram.github.io/devlog/" : "",
+  trailingSlash: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
