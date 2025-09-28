e8f2f6d14086b3249a18110c47cda7a418c044e4
Ahram Kim <ahram0223@gmail.com>
2025-09-29 01:13:51 +0900

    chore: install and setup packages

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+
+        accent: "var(--accent)",
+        accent_foreground: "var(--accent_foreground)",
+
+        muted: "var(--muted)",
+        muted_solid: "var(--muted_solid)",
+        muted_foreground: "var(--muted_foreground)",
+
+        card_bg: "var(--card_bg)",
+        card_solid: "var(--card_solid)",
+
+        border: "var(--border)",
+        input: "var(--input)",
+        code: "var(--code),",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
